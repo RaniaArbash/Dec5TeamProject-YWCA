be5c3691b70538a6def2e4aa057d0b7a003868c9
rania <raniaarbash@hotmail.com>
2025-12-05 15:53:50 -0500

    adding team1&2

--- app/src/main/java/com/example/dec5teamproject/DataLayer/Destinations/RestCountriesApi.kt
+++ app/src/main/java/com/example/dec5teamproject/DataLayer/Destinations/RestCountriesApi.kt
@@ -1,10 +1,10 @@
 package com.example.dec5teamproject.DataLayer.Destinations
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+//import com.squareup.moshi.Moshi
+//import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.http.GET
 
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 interface RestCountriesApi {
     @GET("v3.1/all?fields=name,flags,population,region,currencies")
     suspend fun getAllCountries(): List<CountryDto>
@@ -14,14 +14,14 @@ interface RestCountriesApi {
 //ApiProvider.kt
 
 object ApiProvider {
-    private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory()) // Required for Kotlin data classes
-        .build()
+//    private val moshi = Moshi.Builder()
+//        .add(GsonConverterFactory.create()) // Required for Kotlin data classes
+//        .build()
 
     val restCountriesApi: RestCountriesApi by lazy {
         Retrofit.Builder()
             .baseUrl("https://restcountries.com/")
-            .addConverterFactory(MoshiConverterFactory.create(moshi)) // Pass custom Moshi
+            .addConverterFactory(GsonConverterFactory.create()) // Pass custom Moshi
             .build()
             .create(RestCountriesApi::class.java)
     }
--- app/src/main/java/com/example/dec5teamproject/MainActivity.kt
+++ app/src/main/java/com/example/dec5teamproject/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Dec5TeamProjectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    PopularDestinationsScreen()
+                    ExchangeRateApp()
                 }
             }
         }
